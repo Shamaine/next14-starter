a112e70bd4ad07cffa2c3a9b212d261be22aaeda
Shamaine Chung <shamainechung@gmail.com>
2024-05-02 17:19:36 +0800

    created folder apps and components

---
+++ src/app/blog/layout.jsx
@@ -0,0 +1,11 @@
+// new component shortcut rafce
+const BlogLayout = ({ childen }) => {
+  return (
+    <div>
+      <h2>This is a blog layout</h2>
+    </div>
+  );
+  return <div>BlogLayout</div>;
+};
+
+export default BlogLayout;
