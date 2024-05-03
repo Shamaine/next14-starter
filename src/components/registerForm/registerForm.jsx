e8ea1a1302e9a13d2d4a7c5495a8f3e59aa0aac5
Shamaine Chung <shamainechung@gmail.com>
2024-05-03 17:21:58 +0800

    updated login and register template

---
+++ src/components/registerForm/registerForm.jsx
@@ -0,0 +1,26 @@
+"use client";
+import styles from "./registerForm.module.css";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+
+const RegisterForm = () => {
+  return (
+    <form className={styles.form}>
+      <input type="text" placeholder="username" name="username" />
+      <input type="email" placeholder="email" name="email" />
+      <input type="password" placeholder="password" name="password" />
+      <input
+        type="password"
+        placeholder="password again"
+        name="passwordRepeat"
+      />
+      <button>Register</button>
+      <Link href="/login">
+        Have an account? <b>Login</b>
+      </Link>
+    </form>
+  );
+};
+
+export default RegisterForm;
